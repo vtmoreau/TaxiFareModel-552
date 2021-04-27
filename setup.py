caa7cecd125288a7fd25134069579d3644c0ecaf
Vincent Moreau <vincent.moreau@protonmail.com>
2021-04-27 19:16:28 +0200

    rename

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='TaxiFareModel-552',
+setup(name='TaxiFareModel',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
@@ -13,5 +13,5 @@ setup(name='TaxiFareModel-552',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/TaxiFareModel-552-run'],
+      scripts=['scripts/TaxiFareModel-run'],
       zip_safe=False)
